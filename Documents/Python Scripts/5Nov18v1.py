4711f1799bce0b0475dd7b726f56f40687db5dda
Hany Eldeib <hany.eldeib@campus.viu.edu>
2018-11-05 10:29:23 -0800

    updated newton method program

--- Documents/Python Scripts/5Nov18v1.py
+++ Documents/Python Scripts/5Nov18v1.py
@@ -1,10 +1,10 @@
 import math
 
 def f(x):
-    return (math.tan(x))*(math.log(x))
+    return math.pow(x,4)-3*math.pow(x,3)+2*math.pow(x,2)-1
  
 def df(x):
-    return (math.tan(x))/x + (math.log(x))*(1/(math.pow(math.cos(x),2)))
+    return 4*math.pow(x,3)-9*math.pow(x,2)+4*x
                   
 def dx(f,x):    
     return abs(0-f(x))
@@ -12,9 +12,13 @@ def dx(f,x):
 def newtons_method(f, df, x0, e):
     delta = dx(f, x0)
     while delta > e:
-        x0 = x0 - f(x0)/df(x0)
-        print('current solution is: ', x0)
-        delta = dx(f, x0)
+        if abs(df(x0))>0:
+            x0 = x0 - f(x0)/df(x0)
+            print('current solution is: ', x0)
+            delta = dx(f, x0)
+        else:
+            print('Derivative equals zero')
+            x0=x0+0.1
     print ('Root is at: ', x0, 'f(X) = ', f(x0))
     
 
